6ed280f8b8291a00939b6f48406a34aee13a0120
unknown <alex.unapel@gmail.com>
2024-08-03 11:45:48 -0300

    chore eslint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-const Home = () =>{
-  return <h1 className="text-red-500">Home Page</h1>
-};
+import { Button } from "./_components/ui/button"
 
-export default Home;
+const Home = () => {
+  return <Button>Teste</Button>
+}
+
+export default Home
